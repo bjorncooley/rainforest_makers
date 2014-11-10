771e9781bc28c5067c112c77184780fb172f827a
Bjorn Cooley <bjorncooley@gmail.com>
2014-11-10 14:46:18 -0500

    Inital commit, with functional Spirit app

---
+++ rainforest_makers/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rainforest_makers.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('spirit.urls', namespace="spirit", app_name="spirit")),
+)
